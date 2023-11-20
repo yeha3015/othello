be6d8d9b56f5f89e43adf009cc592414343dc178
yeha3015 <ygoto.acount@gmail.com>
2023-11-20 17:11:57 +0900

    Ver.0.0.2 make flip algolithm(under constructing)

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 from pygame.locals import *
 import sys
 
+# FIXME: リストのインデックスがめちゃくちゃ。ちゃんと整理すべき。
+# 
+
 
 def main():
+    global max
+    max = 7
     pygame.init()
     scsize = 600
-    dsc = scsize // 40
     screen = pygame.display.set_mode((scsize, scsize))
-    stones = [[1, 0, 0, 0, 0, 0, 0, 1],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 2, 0, 0, 0],
-              [0, 0, 0, 2, 1, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 0, 0, 0, 0, 0, 0, 1],]
+    global stones
+    stones = [[9, 9, 9, 9, 9, 9, 9, 9],
+              [9, 9, 9, 9, 9, 9, 9, 9],
+              [9, 9, 9, 9, 9, 9, 9, 9],
+              [9, 9, 9, 0, 1, 9, 9, 9],
+              [9, 9, 9, 1, 0, 9, 9, 9],
+              [9, 9, 9, 9, 9, 9, 9, 9],
+              [9, 9, 9, 9, 9, 9, 9, 9],
+              [9, 9, 9, 9, 9, 9, 9, 9]]
+    turn = 0
     while True:
         make_bg(screen, scsize)
         draw_stones(screen, stones, scsize)
@@ -27,6 +33,14 @@ def main():
                 sys.exit()
             if event.type == MOUSEBUTTONDOWN:
                 cx, cy = event.pos
+                print(cx, cy)
+                nsi, nsj = cx * 8 // scsize, cy * 8 // scsize
+                if stones[nsj][nsi] != 9:
+                    break
+                flip_stones(stones, nsi, nsj, turn)
+                stones[nsj][nsi] = int(turn)
+                turn = not turn
+                print_stones_for_debug(stones)
 
 
 def make_bg(screen, sc):
@@ -41,14 +55,48 @@ def draw_stones(screen, stones, sc):
     dsc = sc // 40
     ctr = (sc - 2 * dsc) // 16
     rnd = ctr - dsc // 2
-    cor = 2
-    for i in range(len(stones)):
-        for j in range(len(stones[i])):
-            if stones[i][j] == 1:
-                pygame.draw.circle(screen, (0, 0, 0), (dsc + ctr*(2*i+1) + cor, dsc + ctr*(2*j+1) + cor), rnd)
-            if stones[i][j] == 2:
-                pygame.draw.circle(screen, (255, 255, 255), (dsc + ctr*(2*i+1) + cor, dsc + ctr*(2*j+1) + cor), rnd)
+    cor = 1.5
+    for i in range(max):
+        for j in range(max):
+            if stones[j][i] == 0:
+                pygame.draw.circle(screen, (0, 0, 0), (dsc + ctr*(2*i+1) + cor*i, dsc + ctr*(2*j+1) + cor*j), rnd)
+            if stones[j][i] == 1:
+                pygame.draw.circle(screen, (255, 255, 255), (dsc + ctr*(2*i+1) + cor*i, dsc + ctr*(2*j+1) + cor*j), rnd)
 
+
+def flip_stones(stones, nsi, nsj, turn):
+    find_opposite_stones_v(stones, nsi, nsj, turn)
+    # print_stones_for_debug(stones)
+    return 0
+
+
+def find_opposite_stones_v(stones, i, j, turn, cnt=0):
+    """
+    fixme: 端に到達したときのコードが動かない。
+    """
+    flag = False
+    flip_from = max
+    # positive way
+    for js in range(max, j, -1):
+        print(flag)
+        if flag:
+            if stones[js][i] == turn:
+                flip_from = js
+            elif stones[js][i] == 9:
+                flag = False
+        else:
+            if stones[js][i] == turn:
+                flag = True
+                flip_from = js
+    if flag:
+        print(i, flip_from)
+        stones[i:flip_from][j] = [int(turn) for _ in range(flip_from - j)]
+    return 0
+        
+
+def print_stones_for_debug(stones):
+    for i in stones:
+        print(i)        
 
 
 if __name__ == "__main__":
